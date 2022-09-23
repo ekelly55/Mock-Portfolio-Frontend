5450b85af4189f4cb4c7fd11801897da44354b96
ekelly55 <eamonn.michael.kellyr@email.com>
2022-09-23 15:34:56 -0400

    delete home import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Routes, Route} from "react-router-dom";
 // IMPORT PAGES
-import Home from "./pages/Home";
+
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
