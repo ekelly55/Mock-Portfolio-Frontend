5c9a2318cec9b0c969aca75bc3306e328c19e527
ekelly55 <eamonn.michael.kellyr@email.com>
2022-10-25 13:21:28 -0400

    restore changes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {Button, Alert, Breadcrumb} from 'react-bootstrap'
+import { Button, Alert, Card, Breadcrumb } from 'react-bootstrap'
 
 function Projects(props) {
   // create state to hold projects
@@ -7,42 +7,54 @@ function Projects(props) {
 
   //create function to make api call
   const getProjectsData = async () => {
-    
-		//make api call and get response
+
+    //make api call and get response
     const response = await fetch(props.URL + "projects");
-    
-		// turn response into javascript object
+
+    // turn response into javascript object
     const data = await response.json();
-    
-		// set the projects state to the data
+
+    // set the projects state to the data
     setProjects(data);
 
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => {getProjectsData()});
+  useEffect(() => { getProjectsData() });
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
-      <div>
-        <h1>{project.name}</h1>
-        <img src={project.image} alt="game" />
-        <div className="link-buttons">
-        <Button className="button m-2" size="sm" as="a" href={project.git} variant="primary">
-          GitHub
-        </Button>
-     
-        <Button className="button" size="sm" as="a" href={project.live} variant="success">
-          Live Site
-        </Button>
-        </div>
-    
-      </div>
-    ));
-  };
+          <div className="col-4">
 
-  return projects ? loaded() : <h1>Loading...</h1>;
+            <Card className="bg-dark text-white ">
+              <Card.Img className="w-50" src={project.image} alt="game" />
+              <Card.ImgOverlay>
+                <Card.Title>{project.name}</Card.Title>
+                <Card.Text>
+                  Describe game here.
+                </Card.Text>
+              </Card.ImgOverlay>
+              <Card.Footer>
+
+              </Card.Footer>
+            </Card>
+            <div className="link-buttons">
+              <Button className="button m-2" size="sm" as="a" href={project.git} variant="primary">
+                GitHub
+              </Button>
+
+              <Button className="button" size="sm" as="a" href={project.live} variant="success">
+                Live Site
+              </Button>
+            </div>
+
+          </div>
+        ));
+      };
+      
+      return projects ? loaded() : <h1>Loading...</h1>;
+  
 }
 
 export default Projects;
